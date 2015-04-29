de6b0b3b6c9ee2a58d830f4c9ca2e8b5bd627a93
Mathieu Ducharme <mat@locomotive.ca>
2015-04-29 15:25:46 -0400

    New Core class: StringFormat. To strip tags, remove accents, or strip non-alphanumerical characters (more to come...)

---
+++ src/Charcoal/Core/StringFormat.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Charcoal\Core;
+
+use \InvalidArgumentException as InvalidArgumentException;
+
+/**
+*
+*/
+class StringFormat
+{
+    /**
+    * Holds the string to format.
+    *
+    * @var string $_string
+    */
+    protected $_string = '';
+
+    /**
+    * Supports unicode or no.
+    *
+    * @var boolean $_unicode
+    */
+    protected $_unicode = true;
+
+    /**
+    * @var string Optional string
+    */
+    public function __construct($string=null)
+    {
+        if($string !== null) {
+            $this->set_string($string);
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->string();
+    }
+
+    /**
+    * @var string $string
+    * @throws InvalidArgumentException
+    * @return StringFormat Chainable
+    */
+    public function set_string($string)
+    {
+        if(!is_string($string)) {
+            throw new InvalidArgumentException('String must be a string');
+        }
+        $this->_string = $string;
+        return $this;
+    }
+
+    /**
+    * @return string
+    */
+    public function string()
+    {
+        return $this->_string;
+    }
+
+    public function set_unicode($unicode)
+    {
+        $this->_unicode = $unicode;
+        return $this;
+    }
+
+    public function unicode()
+    {
+        return $this->_unicode;
+    }
+
+    /**
+    * Strip all HTML tags.
+    * @return StringFormat Chainable
+    */
+    public function strip_tags()
+    {
+        $this->_string = strip_tags($this->_string);
+        return $this;
+    }
+
+    /**
+    * Replace accents with their non-accents counterpats.
+    * @return StringFormat Chainable
+    */
+    public function unaccents()
+    {
+        $str = htmlentities($this->_string, ENT_COMPAT, 'UTF-8');
+        $this->_string = preg_replace('/&([a-zA-Z])(uml|acute|grave|circ|tilde|cedil|ring|caron|slash);/', '$1', $str);
+        $this->_string = preg_replace('/&([a-zA-Z]{2})(lig);/', '$1', $this->_string);
+        return $this;
+    }
+
+    /**
+    * Strip all non-alphanumeric characters from string.
+    *
+    * The definietion of alphanumeric is: 
+    * - In unicode: All letters and numbers
+    * - In non-unicode:
+    *   -Only the 26 letters of the english alphabets (a to z, no accents)
+    *   - Either uppercase or lowercase
+    *   - Plus the number 0 to 9
+    *
+    * Ideally, should be used with unaccents
+    */
+    public function alphanumeric()
+    {
+        if($this->unicode()) {
+            $this->_string = preg_replace("/[^[:alnum:][:space:]]/ui", '', $this->_string);
+        }
+        else {
+            $this->_string = preg_replace("/[^A-Za-z0-9 ]/", '', $this->_string);
+        }
+        return $this;
+    }
+}
---
+++ tests/Charcoal/Core/StringFormatTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Charcoal\Tests\Core;
+
+use \Charcoal\Core\StringFormat as StringFormat;
+
+class StringFormatTest extends \PHPUnit_Framework_TestCase
+{
+
+
+    public function testConstructor()
+    {
+        $obj = new StringFormat();
+        $this->assertInstanceOf('\Charcoal\Core\StringFormat', $obj);
+        $this->assertEquals('', $obj->string());
+        $this->assertTrue($obj->unicode());
+
+        $obj = new StringFormat('Foo bar');
+        $this->assertEquals('Foo bar', $obj->string());
+    }
+
+    public function testMagicString()
+    {
+        $obj = new StringFormat('Foo bar');
+        ob_start();
+        echo $obj;
+        $ret = ob_get_clean();
+        $this->assertEquals('Foo bar', $ret);
+    }
+
+    public function testSetString()
+    {
+        $obj = new StringFormat();
+        $ret = $obj->set_string('Foo bar');
+        $this->assertSame($ret, $obj);
+        $this->assertEquals('Foo bar', $obj->string());
+
+        $this->setExpectedException('\InvalidArgumentException');
+        $obj->set_string(false);
+    }
+
+    public function testSetUnicode()
+    {
+        $obj = new StringFormat();
+        $ret=  $obj->set_unicode(false);
+        $this->assertSame($ret, $obj);
+        $this->assertNotTrue($obj->unicode());
+    }
+
+    public function testStripTags()
+    {
+        $obj = new StringFormat('<p>Test</p>');
+        $ret = $obj->strip_tags();
+        $this->assertSame($ret, $obj);
+        $this->assertEquals('Test', $obj->string());
+    }
+
+    /**
+    * @dataProvider providerUnaccents
+    */
+    public function testUnaccents($str, $res)
+    {
+        $obj = new StringFormat($str);
+        $ret = $obj->unaccents();
+        $this->assertSame($ret, $obj);
+        $this->assertEquals($res, $obj->string());
+    }
+
+    public function providerUnaccents()
+    {
+        return [
+            ['æ œ Œ Æ', 'ae oe OE AE'],
+            ['š', 's'],
+            ['fóø bår FÓØ BÅR', 'foo bar FOO BAR'],
+            ['àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ', 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY']
+        ];
+    }
+
+    public function testAlphanumeric()
+    {
+        $obj = new StringFormat('This "string" contaïns non-àlphanumerical #characters');
+        $ret = $obj->alphanumeric();
+        $this->assertSame($ret, $obj);
+        $this->assertEquals('This string contaïns nonàlphanumerical characters', $obj->string());
+    }
+
+     public function testAlphanumericNonUnicode()
+    {
+        $obj = new StringFormat('This "string" contaïns non-àlphanumerical #characters');
+        $obj->set_unicode(false);
+        $ret = $obj->alphanumeric();
+        $this->assertSame($ret, $obj);
+        $this->assertEquals('This string contans nonlphanumerical characters', $obj->string());
+    }
+}
