0d92aeaa76fab9a3d040cb55b3c310a551438ce3
Mathieu Ducharme <mat@locomotive.ca>
2015-10-07 22:41:52 -0400

    Fully expand the view interface to require all methods.

--- src/Charcoal/View/ViewInterface.php
+++ src/Charcoal/View/ViewInterface.php
@@ -7,6 +7,76 @@ namespace Charcoal\View;
 */
 interface ViewInterface
 {
+    /**
+    * Set the engine type
+    *
+    * @param string $engine_type
+    * @throws InvalidArgumentException
+    * @return AbstractView Chainable
+    */
+    public function set_engine_type($engine_type);
+
+    /**
+    * @return string
+    */
+    public function engine_type();
+
+    /**
+    * @param EngineInterface $engine
+    */
+    public function set_engine(EngineInterface $engine);
+
+    /**
+    * @return EngineInterface
+    */
+    public function engine();
+
+    /**
+    * @return EngineInterface
+    */
+    public function create_engine();
+
+    /**
+    * @param string $template_ident
+    * @throws InvalidArgumentException if the provided argument is not a string
+    * @return AbstractView Chainable
+    */
+    public function set_template_ident($template_ident);
+
+    /**
+    * @return string
+    */
+    public function template_ident();
+
+    /**
+    * @param string $template
+    * @throws InvalidArgumentException if the provided argument is not a string
+    * @return AbstractView Chainable
+    */
+    public function set_template($template);
+
+    /**
+    * @return string
+    */
+    public function template();
+
+    /**
+    * @param string $template_ident
+    * @throws InvalidArgumentException
+    * @return string
+    */
+    public function load_template($template_ident = null);
+
+    /**
+    * @param mixed $context
+    * @return AbstractView Chainable
+    */
+    public function set_context($context);
+
+    /**
+    * @return mixed
+    */
+    public function context();
 
     /**
     * @param string $template
