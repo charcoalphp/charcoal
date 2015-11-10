395bdb9e72bc591ef67753a902020ad0e3717a71
Mathieu Ducharme <mat@locomotive.ca>
2015-11-09 23:03:36 -0500

    Add set_template_path() to PHP loader. Implements LoaderInterface, as intended.

--- src/Charcoal/View/Php/PhpLoader.php
+++ src/Charcoal/View/Php/PhpLoader.php
@@ -2,10 +2,12 @@
 
 namespace Charcoal\View\Php;
 
+use \Charcoal\View\LoaderInterface;
+
 /**
 * The PHP template loader finds a mustache php template file in directories and includes it (run as PHP).
 */
-class PhpLoader
+class PhpLoader implements LoaderInterface
 {
     /**
     * FileLoader > search_path()
@@ -14,9 +16,41 @@ class PhpLoader
     */
     public function search_path()
     {
-        return [
-            '../templates/'
-        ];
+        if (empty($this->search_path)) {
+            return [
+                '../templates/'
+            ];
+        }
+        return $this->search_path;
+    }
+
+        /**
+    * @param array $search_path
+    * @return PhpLoader Chainable
+    */
+    public function set_search_path(array $search_path)
+    {
+        $this->search_path = [];
+        foreach ($search_path as $path) {
+            $this->add_search_path($path);
+        }
+        return $this;
+    }
+
+    /**
+    * @param string $path
+    * @throws InvalidArgumentException
+    * @return PhpLoader Chainable
+    */
+    public function add_search_path($path)
+    {
+        if (!is_string($path)) {
+            throw new InvalidArgumentException(
+                'Path needs to be a string'
+            );
+        }
+        $this->search_path[] = $path;
+        return $this;
     }
 
     /**
@@ -40,7 +74,7 @@ class PhpLoader
         $filename = $this->filename_from_ident($ident);
         $search_path = $this->search_path();
         foreach ($search_path as $path) {
-            $f = $path.'/'.$filename;
+            $f = realpath($path).'/'.$filename;
             if (!file_exists($f)) {
                 continue;
             }
