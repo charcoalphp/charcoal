5413f7ee5d548603b53fa88fef0a8c1e52a22fd2
Mathieu Ducharme <mat@locomotive.ca>
2016-02-04 15:13:59 -0500

    We use nulllogger now.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,4 @@
 <?php
-// Monolog Dependencies
-use \Monolog\Logger;
-use \Monolog\Processor\UidProcessor;
-use \Monolog\Handler\StreamHandler;
 
 $autoloader = require __DIR__.'/../vendor/autoload.php';
 $autoloader->add('Charcoal\\', __DIR__.'/src/');
