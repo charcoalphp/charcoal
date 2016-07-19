90e420ddb7e0c4a36e2b6ccdb9d4d311a5622858
Mathieu Ducharme <mat@locomotive.ca>
2016-07-18 23:19:51 -0400

    Set dependencies from constructor (optionally).

--- src/Charcoal/Ui/AbstractUiItem.php
+++ src/Charcoal/Ui/AbstractUiItem.php
@@ -4,6 +4,9 @@ namespace Charcoal\Ui;
 
 use \InvalidArgumentException;
 
+// Module `pimple` dependencies
+use \Pimple\Container;
+
 // Module `charcoal-config` dependencies
 use \Charcoal\Config\AbstractEntity;
 
@@ -25,4 +28,22 @@ abstract class AbstractUiItem extends AbstractEntity implements UiItemInterface
 {
     use ViewableTrait;
     use UiItemTrait;
+
+    /**
+     * @param array $data Constructor options.
+     */
+    public function __construct(array $data=null)
+    {
+        if (isset($data['container'])) {
+            $this->setDependencies($data['container']);
+        }
+    }
+
+    /**
+     * @param Container $container Pimple DI Container
+     */
+    public function setDependencies(Container $container)
+    {
+        // Void
+    }
 }
