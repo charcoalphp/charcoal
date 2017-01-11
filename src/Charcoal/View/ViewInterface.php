2b34cdea71c74a3c02160d4d0ea84151fa251c6d
Mathieu Ducharme <mat@locomotive.ca>
2017-01-11 10:09:46 -0500

    Simplified Interface. The classes will get much simpler soon.

--- src/Charcoal/View/ViewInterface.php
+++ src/Charcoal/View/ViewInterface.php
@@ -8,58 +8,8 @@ namespace Charcoal\View;
 interface ViewInterface
 {
     /**
-     * @param EngineInterface $engine The rendering engine.
-     * @return ViewInterface Chainable
-     */
-    public function setEngine(EngineInterface $engine);
-
-    /**
-     * @return EngineInterface
-     */
-    public function engine();
-
-    /**
-     * @param string $templateIdent The template ident.
-     * @return AbstractView Chainable
-     */
-    public function setTemplateIdent($templateIdent);
-
-    /**
-     * @return string
-     */
-    public function templateIdent();
-
-    /**
-     * @param string $template The template string.
-     * @return AbstractView Chainable
-     */
-    public function setTemplate($template);
-
-    /**
-     * @return string
-     */
-    public function template();
-
-    /**
-     * @param string $templateIdent The template identifier to load.
-     * @return string The loaded template string.
-     */
-    public function loadTemplate($templateIdent = null);
-
-    /**
-     * Set the rendering context ("view controller").
+     * Load a template (from identifier) and render it.
      *
-     * @param mixed $context The view controller (context).
-     * @return AbstractView Chainable
-     */
-    public function setContext($context);
-
-    /**
-     * @return mixed
-     */
-    public function context();
-
-    /**
      * @param string $templateIdent The template identifier, to load and render.
      * @param mixed  $context       The view controller (rendering context).
      * @return string
@@ -67,6 +17,8 @@ interface ViewInterface
     public function render($templateIdent = null, $context = null);
 
     /**
+     * Render a template (from string).
+     *
      * @param string $templateString The full template string to render.
      * @param mixed  $context        The view controller (rendering context).
      * @return string
