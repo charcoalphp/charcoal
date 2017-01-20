421bfd2a788e0c9f1ec5ee707443a593baf157f6
Dominic Lord <dlord@outlook.com>
2017-01-20 14:40:27 -0500

    Amend 68348ce : Fixing type conversion in comparison

--- src/Charcoal/Property/ObjectProperty.php
+++ src/Charcoal/Property/ObjectProperty.php
@@ -351,7 +351,7 @@ class ObjectProperty extends AbstractProperty implements SelectablePropertyInter
      */
     public function storageVal($val)
     {
-        if ($val === null || $val == '') {
+        if ($val === null || $val === '') {
             // Do not json_encode NULL values
             return null;
         }
