3fa49d82a4d5863ece5e0d9c168e0006dfd105ae
Chauncey McAskill <chauncey@locomotive.ca>
2017-02-08 18:49:16 -0500

    Improved TranslatorHelpers
    Added support for domain, locale, and ‘transChoice’.

--- src/Charcoal/View/Mustache/TranslatorHelpers.php
+++ src/Charcoal/View/Mustache/TranslatorHelpers.php
@@ -27,6 +27,29 @@ class TranslatorHelpers implements HelpersInterface
     private $translator;
 
     /**
+     * Store the given number to use to find the indice of the message.
+     *
+     * Requires {@link https://github.com/bobthecow/mustache.php/wiki/FILTERS-pragma FILTERS pragma}.
+     *
+     * @var integer|null
+     */
+    private $number;
+
+    /**
+     * Store the given locale (Mustache tag node).
+     *
+     * @var string|null
+     */
+    private $locale;
+
+    /**
+     * Store the given domain for the message (Mustache tag node).
+     *
+     * @var string|null
+     */
+    private $domain;
+
+    /**
      * @param array $data Class Dependencies.
      */
     public function __construct(array $data = null)
@@ -45,14 +68,98 @@ class TranslatorHelpers implements HelpersInterface
     public function toArray()
     {
         return [
-            '_t' => function ($text, LambdaHelper $helper) {
-                if ($this->translator) {
-                    $text = $this->translator->trans($text);
+            '_t' => $this
+        ];
+    }
+
+    /**
+     * Clear macros.
+     *
+     * @return void
+     */
+    protected function reset()
+    {
+        $this->number = null;
+        $this->domain = null;
+        $this->locale = null;
+    }
+
+
+
+    // Magic Methods
+    // =========================================================================
+
+    /**
+     * Magic: Render the Mustache section.
+     *
+     * @param  string            $text   The translation key.
+     * @param  LambdaHelper|null $helper For rendering strings in the current context.
+     * @return boolean
+     */
+    public function __invoke($text, LambdaHelper $helper = null)
+    {
+        if (!$helper) {
+            $this->number = $text;
+            return $this;
+        }
+
+        if ($this->translator) {
+            if ($this->number === null) {
+                $text = $this->translator->trans($text, [], $this->domain, $this->locale);
+            } else {
+                if (!is_numeric($this->number) && is_string($this->number)) {
+                    $this->number = $helper->render('{{ '.$this->number.' }}');
                 }
 
-                /** @var string Render any Mustache tags in the translation. */
-                return $helper->render($text);
+                $text = $this->translator->transChoice($text, (int)$this->number, [], $this->domain, $this->locale);
             }
-        ];
+
+            $this->reset();
+        }
+
+        /** @var string Render any Mustache tags in the translation. */
+        return $helper->render($text);
+    }
+
+    /**
+     * Magic: Determine if a property is set and is not NULL.
+     *
+     * Required by Mustache.
+     *
+     * @param  string  $macro A domain, locale, or number.
+     * @return boolean
+     */
+    public function __isset($macro)
+    {
+        return boolval($macro);
+    }
+
+    /**
+     * Magic: Process domain, locale, and number.
+     *
+     * Required by Mustache.
+     *
+     * @param  string $macro A domain, locale, or number.
+     * @return mixed
+     */
+    public function __get($macro)
+    {
+        if (!$this->translator) {
+            return $this;
+        }
+
+        if ($macro === '_t' || $macro === '_n') {
+            return $this;
+        }
+
+        if (in_array($macro, $this->translator->availableLocales())) {
+            $this->locale = $macro;
+        } elseif (in_array($macro, $this->translator->availableDomains())) {
+            $this->domain = $macro;
+        } else {
+            $this->number = $macro;
+        }
+
+        return $this;
     }
 }
