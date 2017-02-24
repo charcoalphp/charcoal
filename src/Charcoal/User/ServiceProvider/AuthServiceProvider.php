939855f3c0ea56482c84926effa7d2920464f689
Chauncey McAskill <chauncey@locomotive.ca>
2017-02-24 17:39:53 -0500

    Added Generic AuthServiceProvider

---
+++ src/Charcoal/User/ServiceProvider/AuthServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Charcoal\User\ServiceProvider;
+
+// From 'zendframework/zend-permissions-acl'
+use Zend\Permissions\Acl\Acl;
+
+// From Pimple
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+// From 'charcoal-user'
+use Charcoal\User\Authenticator;
+use Charcoal\User\Authorizer;
+use Charcoal\User\AuthToken;
+use Charcoal\User\GenericUser as User;
+
+/**
+ *
+ */
+class AuthServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @param  Container $container A Pimple DI container.
+     * @return void
+     */
+    public function register(Container $container)
+    {
+        /**
+         * @param  Container $container The Pimple DI Container.
+         * @return Authenticator
+         */
+        $container['authenticator'] = function (Container $container) {
+            return new Authenticator([
+                'logger'        => $container['logger'],
+                'user_type'     => User::class,
+                'user_factory'  => $container['model/factory'],
+                'token_type'    => AuthToken::class,
+                'token_factory' => $container['model/factory']
+            ]);
+        };
+
+        /**
+         * @param  Container $container The Pimple DI container.
+         * @return Authorizer
+         */
+        $container['authorizer'] = function (Container $container) {
+            return new Authorizer([
+                'logger'    => $container['logger'],
+                'acl'       => new Acl(),
+                'resource'  => 'charcoal'
+            ]);
+        };
+    }
+}
