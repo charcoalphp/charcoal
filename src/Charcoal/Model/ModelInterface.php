ab9aadca30482212bc0965efbb815f59478e209b
Mathieu Ducharme <mat@locomotive.ca>
2017-12-03 16:18:21 -0500

    ModelInterface now inherits EntityInterface

--- src/Charcoal/Model/ModelInterface.php
+++ src/Charcoal/Model/ModelInterface.php
@@ -2,39 +2,14 @@
 
 namespace Charcoal\Model;
 
-// From 'charcoal-property'
-use Charcoal\Property\PropertyInterface;
+// From `charcoal-config`
+use Charcoal\Config\EntityInterface;
 
 /**
  * Describes a data model.
  */
-interface ModelInterface
+interface ModelInterface extends EntityInterface
 {
-    /**
-     * Set the model data.
-     *
-     * Example:
-     * ```
-     * {
-     *     "title": {
-     *         "en": "Charcoal",
-     *         "fr": "Charbon"
-     *     }
-     * }
-     * ```
-     *
-     * @param  array $data The model data.
-     * @return ModelInterface Returns the current model.
-     */
-    public function setData(array $data);
-
-    /**
-     * Retrieve the model data as a structure.
-     *
-     * @return array
-     */
-    public function data();
-
     /**
      * Set the model data (from a flattened structure).
      *
@@ -72,7 +47,7 @@ interface ModelInterface
      *
      * @param  array $propertyIdents Optional. List of property identifiers
      *     for retrieving a subset of property objects.
-     * @return PropertyInterface[]
+     * @return \Charcoal\Property\PropertyInterface[]
      */
     public function properties(array $propertyIdents = null);
 
@@ -80,8 +55,8 @@ interface ModelInterface
      * Retrieve an instance of {@see PropertyInterface} for the given property.
      *
      * @param  string $propertyIdent The property (ident) to get.
-     * @throws InvalidArgumentException If the property identifier is invalid.
-     * @return PropertyInterface
+     * @throws \InvalidArgumentException If the property identifier is invalid.
+     * @return \Charcoal\Property\PropertyInterface
      */
     public function property($propertyIdent);
 
@@ -89,7 +64,7 @@ interface ModelInterface
      * Alias of {@see ModelInterface::property()} and {@see ModelInterface::properties()}.
      *
      * @param  string|null $propertyIdent Optional property identifier.
-     * @return PropertyInterface|PropertyInterface[] If $propertyIdent is NULL,
+     * @return \Charcoal\Property\PropertyInterface|\Charcoal\Property\PropertyInterface[] If $propertyIdent is NULL,
      *     returns all properties. Otherwise, returns the property associated with $propertyIdent.
      */
     public function p($propertyIdent = null);
@@ -98,7 +73,7 @@ interface ModelInterface
      * Determine if the model has the given property.
      *
      * @param  string $propertyIdent The property identifier to lookup.
-     * @throws InvalidArgumentException If the property identifier is invalid.
+     * @throws \InvalidArgumentException If the property identifier is invalid.
      * @return boolean
      */
     public function hasProperty($propertyIdent);
