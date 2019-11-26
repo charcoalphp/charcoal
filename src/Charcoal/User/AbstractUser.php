8350e7aa684407166fc0270c821c720f7af1860d
Bene <roch.bene@gmail.com>
2019-11-26 15:41:43 -0500

    Fix user validation when ID is not yet saved in the DB to prevent error when creating abstract users from the back-end

--- src/Charcoal/User/AbstractUser.php
+++ src/Charcoal/User/AbstractUser.php
@@ -531,7 +531,7 @@ abstract class AbstractUser extends Content implements
 
         $originalUser = $factory->create($objType)->load($this->getAuthId());
 
-        if ($originalUser->getAuthIdentifier() !== $userLogin) {
+        if ($originalUser->id() && $originalUser->getAuthIdentifier() !== $userLogin) {
             $existingUser = $factory->create($objType)->loadFrom($userKey, $userLogin);
             /** Check for existing user with given email. */
             if (!empty($existingUser->getAuthId())) {
