1e0e22581475721e4e16bfa9ef329c06aee684d1
Mathieu Ducharme <mat@locomotive.ca>
2020-04-20 16:37:18 -0400

    Also upgrade build scripts to php7

--- build/travis/phpdoc_ghpages.sh
+++ build/travis/phpdoc_ghpages.sh
@@ -8,7 +8,7 @@
 # echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 # echo "TRAVIS_BUILD_NUMBER: $TRAVIS_BUILD_NUMBER"
 
-if [ "$TRAVIS_REPO_SLUG" == "locomotivemtl/charcoal-view" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PHP_VERSION" == "5.6" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "locomotivemtl/charcoal-view" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PHP_VERSION" == "7.3" ]; then
 
     echo -e "Publishing PHPDoc to GitHub Pages...\n"
 
