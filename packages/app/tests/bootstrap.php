18b5923708f891751fcefa843353ca36822fc75c
Joel Alphonso <joel@locomotive.ca>
2022-09-06 17:37:18 -0400

    test(charcoal): change the way bootstrap files are loaded based on environment variable
    this allows to use package specific bootstrap files while being aware of the running environment

--- packages/app/tests/bootstrap.php
+++ packages/app/tests/bootstrap.php
@@ -4,5 +4,9 @@ session_start();
 mb_internal_encoding('UTF-8');
 date_default_timezone_set('UTC');
 
-/** @var \Composer\Autoload\ClassLoader $autoloader */
-$autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
+if (($_ENV['TEST_MODE'] ?? '') === 'PACKAGE') {
+    require getcwd().'/tests/bootstrap.php';
+} else {
+    /** @var \Composer\Autoload\ClassLoader $autoloader */
+    $autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Assigin the Composer autoloader to the global state.
+ * Assign the Composer autoloader to the global state.
  *
  * @see \Charcoal\Tests\Config\Mixin\FileLoader\YamlFileLoaderTest::testAddFileWithNoYamlParser()
  *     The autoloader is needed to temporarily remove the "symfony/yaml" package
- *     in order to test the Config's behaviour when faced with a missiong YAML parser.
+ *     in order to test the Config's behaviour when faced with a missing YAML parser.
  *
  * @var \Composer\Autoload\ClassLoader $autoloader
  */
