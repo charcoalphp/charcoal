1c8c66bd3c79dc388b6fa5f3751d6cbdfa03da17
Joel Alphonso <joel@locomotive.ca>
2022-09-09 11:03:51 -0400

    fix(monorepo): implement a custom UpdateReplace release worker to prevent overwriting the whole composer.json 'replace' section

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -2,55 +2,32 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
-use Symplify\MonorepoBuilder\ValueObject\Option;
+use Charcoal\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
+use Symplify\MonorepoBuilder\Config\MBConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-
+return static function (MBConfig $mbConfig): void {
     // where are the packages located?
-    $parameters->set(Option::PACKAGE_DIRECTORIES, [
+    $mbConfig->packageDirectories([
         // default value
         __DIR__.'/packages',
     ]);
 
     // for "merge" command.
-    $parameters->set(Option::DATA_TO_APPEND, [
+    $mbConfig->dataToAppend([
         ComposerJsonSection::REQUIRE_DEV => [
             'phpunit/phpunit' => '^9.5',
         ],
-        ComposerJsonSection::REPLACE => [
-            'locomotivemtl/charcoal-admin' => '*',
-            'locomotivemtl/charcoal-app' => '*',
-            'locomotivemtl/charcoal-attachment' => '*',
-            'locomotivemtl/charcoal-cache' => '*',
-            'locomotivemtl/charcoal-cms' => '*',
-            'locomotivemtl/charcoal-config' => '*',
-            'locomotivemtl/charcoal-core' => '*',
-            'locomotivemtl/charcoal-email' => '*',
-            'locomotivemtl/charcoal-factory' => '*',
-            'locomotivemtl/charcoal-image' => '*',
-            'locomotivemtl/charcoal-object' => '*',
-            'locomotivemtl/charcoal-property' => '*',
-            'locomotivemtl/charcoal-queue' => '*',
-            'locomotivemtl/charcoal-translator' => '*',
-            'locomotivemtl/charcoal-ui' => '*',
-            'locomotivemtl/charcoal-user' => '*',
-            'locomotivemtl/charcoal-view' => '*',
-        ],
     ]);
 
-    $services = $containerConfigurator->services();
-
     # release workers - in order to execute
-    $services->set(UpdateReplaceReleaseWorker::class);
-    $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
-
-    $services->set(SetNextMutualDependenciesReleaseWorker::class);
-    $services->set(UpdateBranchAliasReleaseWorker::class);
+    $mbConfig->workers([
+        UpdateReplaceReleaseWorker::class,
+        SetCurrentMutualDependenciesReleaseWorker::class,
+        SetNextMutualDependenciesReleaseWorker::class,
+        UpdateBranchAliasReleaseWorker::class,
+    ]);
 };
