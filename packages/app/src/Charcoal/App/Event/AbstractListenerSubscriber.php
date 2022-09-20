b382222ef1f71ac065a0033cd12b1d609e23d222
Joel Alphonso <joel@locomotive.ca>
2022-09-20 14:20:41 -0400

    feat(event): add support for ListenerSubscriber through config files and add AbstractListenerSubscriber.php

---
+++ packages/app/src/Charcoal/App/Event/AbstractListenerSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Charcoal\App\Event;
+
+use Charcoal\Factory\FactoryInterface;
+use League\Event\ListenerRegistry;
+use League\Event\ListenerSubscriber;
+use Pimple\Container;
+
+/**
+ * Base class for listener Subscriber
+ *
+ * Subscribers are classes that allows registering multiple event listeners in grouped and organized manner.
+ */
+abstract class AbstractListenerSubscriber implements ListenerSubscriber
+{
+    protected FactoryInterface $listenerFactory;
+
+    /**
+     * @param Container $container
+     * @return void
+     */
+    public function setDependencies(Container $container)
+    {
+        $this->listenerFactory = $container['event/listener/factory'];
+    }
+
+    /**
+     * @param $listener
+     * @return EventListenerInterface
+     */
+    protected function createListener($listener): EventListenerInterface
+    {
+        return $this->listenerFactory->create($listener);
+    }
+
+    abstract public function subscribeListeners(ListenerRegistry $acceptor): void;
+}
