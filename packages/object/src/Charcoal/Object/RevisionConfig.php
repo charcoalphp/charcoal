8bffba819d7e7e13478f9f23410e9b9310acbaee
Joel Alphonso <joel@locomotive.ca>
2022-09-26 16:07:55 -0400

    feat(revision): add RevisionConfig.php to cast revisions config nodes to

---
+++ packages/object/src/Charcoal/Object/RevisionConfig.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Charcoal\Object;
+
+use Charcoal\Config\AbstractConfig;
+
+/**
+ * Revision Config
+ *
+ * The config loaded when creating a revision for a model.
+ * The config is generated from the `revisions` key in the config and can be customized per model.
+ *
+ * `'revisions' : {'Namespace\\Model: {...}'}` : here the `...` represents the data used to create the config.
+ */
+class RevisionConfig extends AbstractConfig
+{
+    protected bool $enabled = true;
+    protected string $revisionClass = ObjectRevision::class;
+}
