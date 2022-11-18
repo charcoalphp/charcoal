310eb8b5b69ddb40f957c17000d7248fa327bc3c
Chauncey McAskill <chauncey@locomotive.ca>
2022-11-18 16:57:21 -0500

    Rename parameter name in RevisionServiceProvider
    Despite `$pimple` being the correct parameter name [1], using `$container` falls in line with all other occurrences in service providers and elsewhere.
    
    The inconsistent parameter can pose problems with PHP 8's named arguments and for static analysis tools like PHPStan and Psalm which will flag this as an error.
    
    This can be corrected in a future changeset that replaces all type-hints of Pimple's Container with the PSR-11 Container interface.
    
    [1]: https://github.com/silexphp/Pimple/blob/v3.5.0/src/Pimple/ServiceProviderInterface.php#L43
    
    Co-authored-by: Xavier Aymond <xavier.aymond@hotmail.fr>

--- packages/object/src/Charcoal/Object/RevisionServiceProvider.php
+++ packages/object/src/Charcoal/Object/RevisionServiceProvider.php
@@ -11,10 +11,10 @@ use Pimple\ServiceProviderInterface;
  */
 class RevisionServiceProvider implements ServiceProviderInterface
 {
-    public function register(Container $pimple)
+    public function register(Container $container)
     {
-        $pimple['revisions/config'] = function (Container $pimple): RevisionsConfig {
-            $configData = $pimple['config']->get('revisions');
+        $container['revisions/config'] = function (Container $container): RevisionsConfig {
+            $configData = $container['config']->get('revisions');
 
             // If the config data is a boolean, it means we only want to affect the enabled state.
             if (is_bool($configData)) {
@@ -26,8 +26,8 @@ class RevisionServiceProvider implements ServiceProviderInterface
             return new RevisionsConfig($configData);
         };
 
-        $pimple['revisions/manager'] = function (Container $pimple): RevisionsManager {
-            $services = new ServiceLocator($pimple, [
+        $container['revisions/manager'] = function (Container $container): RevisionsManager {
+            $services = new ServiceLocator($container, [
                 'revisions/config',
                 'model/factory',
                 'logger'
