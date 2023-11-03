d0d600678fd8c3e43c9b05b6874ca3ba475ec034
Chauncey McAskill <chauncey@locomotive.ca>
2023-11-03 12:48:47 -0400

    Fix camelization of form properties in StructureFormGroup
    Delay camelization of the property key until it is known.
    
    The `groupProperties()` method usually returns a list of property keys (`list<string>`) but the `parsedFormProperties()` method is designed to also expect a map of properties (`array<string, mixed>`) just in case.

--- packages/admin/src/Charcoal/Admin/Widget/FormGroup/StructureFormGroup.php
+++ packages/admin/src/Charcoal/Admin/Widget/FormGroup/StructureFormGroup.php
@@ -466,17 +466,18 @@ class StructureFormGroup extends FormGroupWidget implements
         if ($this->parsedFormProperties === null) {
             $this->finalizeStructure();
 
-            $groupProperties     = array_map([ $this, 'camelize' ], $this->groupProperties());
+            $groupProperties     = $this->groupProperties();
             $availableProperties = $this->structProperties();
 
             $structProperties = [];
             if (!empty($groupProperties)) {
                 foreach ($groupProperties as $propertyIdent => $propertyMetadata) {
-                    $propertyIdent = $this->camelize($propertyIdent);
                     if (is_string($propertyMetadata)) {
                         $propertyIdent    = $propertyMetadata;
                         $propertyMetadata = null;
                     }
+
+                    $propertyIdent = $this->camelize($propertyIdent);
 
                     if (!isset($availableProperties[$propertyIdent])) {
                         continue;
