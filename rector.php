dc3f349fc01b8b9306a488ef9cc8c8fad6874ad7
Joel Alphonso <info@joelalphonso.com>
2026-05-19 15:54:54 -0400

    chore(rector): add Rector configuration for PHP 8.5 and PHPUnit upgrades

---
+++ rector.php
@@ -0,0 +1,32 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withComposerBased(phpunit: true, symfony: true)
+    ->withPaths([
+        __DIR__ . '/tests',
+        __DIR__ . '/packages/*/src',
+        __DIR__ . '/packages/*/tests',
+    ])->withSkip([
+        __DIR__ . '/packages/*/tests/*/*/Fixture/*',
+    ])
+    ->withPhpVersion(PhpVersion::PHP_85)
+    ->withSets([
+        \Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_85,
+        PHPUnitSetList::PHPUNIT_100,
+        PHPUnitSetList::PHPUNIT_110,
+        PHPUnitSetList::PHPUNIT_120
+    ])
+    ->withRules([
+//        SerializableToSerializeRector::class
+//        \Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector::class,
+//        \Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationBasedOnParentClassMethodRector::class,
+    ])
+    ->withConfiguredRule(\Rector\Removing\Rector\Class_\RemoveInterfacesRector::class, [
+        'Serializable',
+    ])
+    ->withPreparedSets(typeDeclarations: true, deadCode: true, codeQuality: true)
+    ->withPhpSets(php85: true);
