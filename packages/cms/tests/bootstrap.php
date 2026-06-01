247a6cd11bda6130b4f131e7fc83105113757911
Joel Alphonso <info@joelalphonso.com>
2026-06-01 12:04:50 -0400

    tests: update PHPUnit configurations to use bootstrap scripts and modernize schema versions

---
+++ packages/cms/tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+session_start();
+mb_internal_encoding('UTF-8');
+date_default_timezone_set('UTC');
+
+if (($_ENV['TEST_MODE'] ?? '') === 'PACKAGE') {
+    require getcwd().'/tests/bootstrap.php';
+} else {
+    /** @var \Composer\Autoload\ClassLoader $autoloader */
+    $autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
+}
