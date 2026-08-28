1277b5ce897d7754a5ce211a4ae0c2e56501b40e
Mathieu Ducharme <mat@locomotive.ca>
2026-08-28 15:58:39 -0400

    fix(user): Regenerate session ID on active session. (#98)

--- packages/user/src/Charcoal/User/AbstractAuthenticator.php
+++ packages/user/src/Charcoal/User/AbstractAuthenticator.php
@@ -384,6 +384,10 @@ abstract class AbstractAuthenticator implements
             return;
         }
 
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            session_regenerate_id(true);
+        }
+
         $this->updateUserSession($user);
 
         if ($remember) {
