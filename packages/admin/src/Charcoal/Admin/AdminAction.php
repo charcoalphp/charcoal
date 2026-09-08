5bbf85539bb92202170e8bdc22fbe35c8c7879d3
Mathieu Ducharme <mat@locomotive.ca>
2026-09-07 22:06:53 -0400

    fix: use guzzle for recaptcha

--- packages/admin/src/Charcoal/Admin/AdminAction.php
+++ packages/admin/src/Charcoal/Admin/AdminAction.php
@@ -2,12 +2,15 @@
 
 namespace Charcoal\Admin;
 
+use Exception;
 use RuntimeException;
 // From PSR-7
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 // From Pimple
 use Pimple\Container;
+// From 'guzzlehttp/guzzle'
+use GuzzleHttp\Client as GuzzleClient;
 // From 'charcoal-factory'
 use Charcoal\Factory\FactoryInterface;
 // From 'charcoal-user'
@@ -356,10 +359,21 @@ abstract class AdminAction extends AbstractAction implements
 
         $this->logger->debug(sprintf('Verifying reCAPTCHA user response: %s', $url));
 
-        /**
-         * @todo Use Guzzle
-         */
-        $result = file_get_contents($url);
+        $guzzleClient = new GuzzleClient();
+
+        try {
+            $response = $guzzleClient->request('GET', $url, [
+                'http_errors' => false,
+            ]);
+            $result = (string)$response->getBody();
+        } catch (Exception $e) {
+            $this->logger->error(sprintf(
+                'Google reCAPTCHA verification request failed: %s',
+                $e->getMessage()
+            ));
+            $result = null;
+        }
+
         $result = (array)json_decode($result, true);
 
         $this->recaptchaLastToken  = $token;
